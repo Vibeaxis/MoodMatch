7121058b51f407dd5bf749915fe6b796683506f5
Reality Patchnotes <92701089+Vibeaxis@users.noreply.github.com>
2026-01-26 00:46:02 -0500

    Update base path in Vite configuration
    Set base path for GitHub Pages deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MoodMatch/', // CRITICAL for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
